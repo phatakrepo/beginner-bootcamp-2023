1ac4b8ad9e6eb2dd1a1cb1275a7880c384a445de
phatakrepo <37370321+phatakrepo@users.noreply.github.com>
2023-09-24 12:28:13 +0530

    change structure of root module and add (#27)

--- main.tf
+++ main.tf
@@ -1,21 +1,4 @@
-terraform {
-  cloud {
-    organization = "terraform_2023_bootcamp"
-    workspaces {
-      name = "terrahouse-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
+
 
 resource "random_string" "bucket_name" {
   length           = 32
@@ -26,8 +9,8 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name_result" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid  = var.user_uuid
+ }
 }
