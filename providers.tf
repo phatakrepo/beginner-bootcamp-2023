26a551969fa658e56210adebd4dc3574b1b2f8fa
phatakrepo <37370321+phatakrepo@users.noreply.github.com>
2023-09-24 13:41:05 +0530

    bucket should no longer use random provider (#29)

--- providers.tf
+++ providers.tf
@@ -1,18 +1,14 @@
 terraform {
+  required_providers {
  # cloud {
  #   organization = "terraform_2023_bootcamp"
  #  workspaces {
  #     name = "terrahouse-1"
  #   }
  # }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
+   aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
     }
-  }
+  }  
 }
