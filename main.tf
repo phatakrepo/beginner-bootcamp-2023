3b0e6af650004dd63fb1ba58825903f9027a142f
phatakrepo <37370321+phatakrepo@users.noreply.github.com>
2023-09-26 16:19:18 +0530

    enabled static website hosting uploaded index and error.html files via terraform (#33)

--- main.tf
+++ main.tf
@@ -16,4 +16,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
+  index_html_path = var.index_html_path
+  error_html_path = var.error_html_path
 }
