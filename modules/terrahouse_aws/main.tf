62f3b560ae7cd4cb7bf6cadca31e82d477a548ef
phatakrepo <37370321+phatakrepo@users.noreply.github.com>
2023-09-27 16:25:48 +0530

    Created cdn and serve s3 bucket from cdn (#35)

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -13,28 +13,4 @@ resource "aws_s3_bucket" "website_bucket" {
     UserUuid  = var.user_uuid
  }
 }
-
-resource "aws_s3_bucket_website_configuration" "website_configuration" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  source = "${path.root}/public/index.html"
-  key    = "index.html"
-  etag = filemd5("${path.root}/public/index.html")
-}
-
-resource "aws_s3_object" "error_html" {
-  bucket = aws_s3_bucket.website_bucket.bucket
-  source =  var.error_html_path
-  key    = "error.html"
-    etag = filemd5(var.error_html_path)
-}
+data "aws_caller_identity" "current" {}
---
+++ modules/terrahouse_aws/resource-storage.tf
@@ -0,0 +1,50 @@
+resource "aws_s3_bucket_website_configuration" "website_configuration" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  source = "${path.root}/public/index.html"
+  key    = "index.html"
+  content_type = "text/html"  # Set the content type here
+  etag = filemd5("${path.root}/public/index.html")
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  source =  var.error_html_path
+  key    = "error.html"
+  content_type = "text/html"  # Set the content type here
+  etag = filemd5(var.error_html_path)
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
+   policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowCloudFrontServicePrincipalReadOnly",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.website_bucket.id}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:${aws_cloudfront_distribution.s3_distribution.id}"
+          }
+        }
+      }
+    ]
+  })
+}
