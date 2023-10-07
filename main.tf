9f80caf92498a13ac9e31db729abc3bb6d7fdd69
Salil Phatak <phatak.salil@gmail.com>
2023-10-07 04:21:46 +0000

    Finished implementing terraform provider

--- main.tf
+++ main.tf
@@ -31,3 +31,15 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+DESCRIPTION
+  domain_name = module.home_arcanum_hosting.domain_name
+  town = "missingo"
+  content_version = var.arcanum.content_version
+}
